6cb57e2d80e032e1445b6701204e7c779c18f81a
Iago Rosal <iago.rosal@live.com>
2025-08-27 23:09:59 -0300

    Primeiro Arquivo

---
+++ Tp_dados.py
@@ -0,0 +1,21 @@
+inteiro = 10
+variavel = 9.9
+nome = "Iago"
+
+print(f'Está é uma variavel do tipo: {type(inteiro)}')
+print(f'Está é uma variavel do tipo: {type(variavel)}')
+print(f'Está é uma variavel do tipo: {type(nome)}')
+
+print('-'*80)
+
+verdadeiro = True
+
+print(f'está uma variavel boleana {type(verdadeiro)}')
+
+print('-'*80)
+
+
+dic = {"nome": "Iago", "idade":"32", "altura":"1.75"}
+
+
+print(dic)

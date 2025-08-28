d03836a545563dc1e989218e0375fc4bf4493c40
Iago Rosal <iago.rosal@live.com>
2025-08-27 23:31:19 -0300

    Criado novo arquivo

---
+++ variaveis.py
@@ -0,0 +1,23 @@
+nome ='Iago'
+idade = 32
+cidade = "São Paulo"
+
+print(f'Olá, me chamo {nome} e tenho {idade} e moro na cidade de {cidade}')
+
+print("-"*80)
+
+a = 10
+b = 20
+print(a,b)
+
+a,b = b, a
+
+print(a,b)
+
+print("-"*80)
+
+valor = 100
+
+valor += 25
+
+print(valor)
